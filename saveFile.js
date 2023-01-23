a5e64417920252cf21311cbc7486909d8e6b41cf
HenrikSaggis <Henrik_ES@hotmail.no>
2023-01-23 13:20:20 +0000

    endret noen kommentarer

--- saveFile.js
+++ saveFile.js
@@ -1,9 +1,9 @@
 function saveToFile() {
-    // Get the selected classification value
+    // Get the selected classification value for the report.
     var classificationSelect = document.querySelector(".classification select");
     var classification = classificationSelect.options[classificationSelect.selectedIndex].value;
 
-    // Get the input values from the text fields
+    // Get the static values from the input fields in the report.
     var from = document.getElementById("from").value;
     var to = document.getElementById("to").value;
     var writerOperator = document.getElementById("writeroperator").value;
@@ -16,16 +16,12 @@ function saveToFile() {
     var journalNumberFields = document.querySelectorAll("#journalnumber");
     var journalNumbers = [];
     
+    // LEGGE TIL FORKLARENDE KOMMENTAR HER.
     for (var i = 0; i < journalNumberFields.length; i++) {
         journalNumbers.push(journalNumberFields[i].value);
     }
-    console.log(journalNumberFields)
-    // Get the facts, comment, and assessment input fields
-    //var facts = document.getElementById("facts").value;
-    //var comment = document.getElementById("comment").value;
-    //var assessment = document.getElementById("assessment").value;
 
-    // Create the text file content
+    // Create the content for the report.
     var fileContent = "Classification: " + classification + "\n\n"
         + "From: " + from + "\n"
         + "To: " + to + "\n"
@@ -35,10 +31,11 @@ function saveToFile() {
         + "Pattern of life: " + patternOfLife + "\n\n"
         + "Static units in NAI: " + staticUnits + "\n\n";
 
-        for (var i = 0; i < journalNumbers.length; i++) {
-            var factsID = 'facts' + i;
-            var commentID = 'comment' + i;
-            var assessmentID = 'assessment' + i;
+
+    for (var i = 0; i < journalNumbers.length; i++) {
+        var factsID = 'facts' + i;
+        var commentID = 'comment' + i;
+        var assessmentID = 'assessment' + i;
 
             var newFacts = document.getElementById(factsID).value;
             var newComment = document.getElementById(commentID).value;
@@ -66,54 +63,3 @@ function saveToFile() {
     // Remove the link element from the body
     document.body.removeChild(downloadLink);
 }
-
-/*function saveToFile() {
-    // Get the values from the text fields
-    var classification = document.querySelector("select").value;
-    var from = document.getElementById("from").value;
-    var to = document.getElementById("to").value;
-    var writeroperator = document.getElementById("writeroperator").value;
-    var ownposition = document.getElementById("ownposition").value;
-    var bluf = document.getElementById("bluf").value;
-    var patternoflife = document.getElementById("patternoflife").value;
-    var staticunits = document.getElementById("staticunits").value;
-    var journalnumber = document.getElementById("journalnumber").value;
-    var facts = document.getElementById("facts").value;
-    var comment = document.getElementById("comment").value;
-    var assessment = document.getElementById("assessment").value;
-
-    // Create an object with the values
-    var object = {
-        classification: classification,
-        from: from,
-        to: to,
-        writeroperator: writeroperator,
-        ownposition: ownposition,
-        bluf: bluf,
-        patternoflife: patternoflife,
-        staticunits: staticunits,
-        journalnumber: journalnumber,
-        facts: facts,
-        comment: comment,
-        assessment: assessment
-    };
-
-    // Convert the object to a JSON string
-    var jsonString = JSON.stringify(object);
-
-    // Create a Blob object with the JSON string and the appropriate MIME type
-    var blob = new Blob([jsonString], {type: "application/json"});
-
-    // Create a link element with the blob object as the href
-    var link = document.createElement("a");
-    link.href = URL.createObjectURL(blob);
-    link.download = "data.json";
-
-    // Append the link element to the body and click it to trigger the download
-    document.body.appendChild(link);
-    link.click();
-
-    // Remove the link element from the body
-    document.body.removeChild(link);
-}
-*/
