9e19c8ac9ba05917460a744cdcec79c24a504547
HenrikSaggis <Henrik_ES@hotmail.no>
2023-01-26 09:27:30 +0000

    making some progress

--- scripts/uploadFromFile.js
+++ scripts/uploadFromFile.js
@@ -1,30 +1,33 @@
 
-let uploadedFileAsArray = [];
+let uploadedFileAsString = "";
 
 
 function uploadFromFile() {
-    const content = document.querySelector('.content');
+    // const content = document.querySelector('.content');
     const [file] = document.querySelector('input[type=file]').files;
     const reader = new FileReader();
   
     reader.addEventListener("load", () => {
       // this will then display a text file
-      content.innerText = reader.result;
-      uploadedFileAsArray.push(reader.result);
+      //content.innerText = reader.result;
+      uploadedFileAsString = reader.result;
+      console.log(uploadedFileAsString);
+
+      var stringSplitted = uploadedFileAsString.split(/\r?\n/);
+      console.log(stringSplitted);
+
+      // Extract the classification from text file, and set the classification option to the correct option.
+      var allClassifications = document.querySelector(".classification select");
+      var allClassificationsArray = [];
+      let reportClassification = uploadedFileAsString.slice(16,19);
+      console.log(reportClassification);
+
+      for (var i = 1; i < 5; i++) {
+        allClassificationsArray.push(allClassifications.options[i].value);
+      }
+
     }, false);
-  
     if (file) {
       reader.readAsText(file);
     }
-
-    //uploadedFileAsArray.push("test");
-    //uploadedFileAsArray.push("test2");
-
-    var text = ["This is some text. \n This is a line break"];
-    
-    console.log(uploadedFileAsArray);
-    console.log(text);
-
-
-
 }
