9be5ad3d4eed52946385b2cb50627bfcd18b890e
egilno <notoegil@gmail.com>
2023-01-30 17:45:29 +0100

    Opprettet O-05

---
+++ scripts/saveFileO05.js
@@ -0,0 +1,91 @@
+function saveToFileO05() {
+    // Get the selected classification value for the report.
+    var classificationSelect = document.querySelector(".classification select");
+    var classification = classificationSelect.options[classificationSelect.selectedIndex].value;
+  
+    // Get the static values from the input fields in the report.
+    var from = document.getElementById("from").value;
+    var to = document.getElementById("to").value;
+    var writerOperator = document.getElementById("writeroperator").value;
+    var ownPosition = document.getElementById("ownposition").value;
+    var dtgOfObservation = document.getElementById("dtg0").value;
+    var status = document.getElementById("status0").value;
+    var enemyPosition = document.getElementById("enemyposition0").value;
+    var actionSinceLast = document.getElementById("actionsincelast0").value;
+    var ownPlan = document.getElementById("ownplan0").value;
+    var additionalInfo = document.getElementById("additionalinfo0").value;
+
+    // Create the content for the report.
+    var fileContent = "Classification: " + classification + "\n\n"
+        + "From: " + from + "\n"
+        + "To: " + to + "\n"
+        + "Writer/Operator: " + writerOperator + "\n"
+        + "Own position: " + ownPosition + "\n"
+        + "DTG: " + dtgOfObservation + "\n\n"
+        + "Status: " + status + "\n"
+        + "Enemy position: " + enemyPosition + "\n"
+        + "Actions since last SITREP: " + actionSinceLast + "\n"
+        + "Own plan of action: " + ownPlan + "\n"
+        + "Additional info: " + additionalInfo + "\n"
+  
+    // Get the date and time from the local computer.
+    var newDate = new Date();
+    var year = newDate.getFullYear();
+    var month = newDate.getMonth();
+    var day = newDate.getDate();
+    var hour = newDate.getHours();
+    var minute = newDate.getMinutes();
+  
+    // Check if values are less than 10, and format them correctly if true.
+    if (month < 10) {
+      month = "0" +(newDate.getMonth() + 1);
+    }
+    else {
+      month = (newDate.getMonth() + 1);
+    }
+    if (day < 10) {
+      date = "0" + newDate.getDate();
+    }
+    else {
+      date = newDate.getDate();
+    }
+    if (hour < 10) {
+      hour = "0" + newDate.getHours();
+    }
+    else {
+      hour = newDate.getHours();
+    }
+    if (minute < 10) {
+      minute = "0" + newDate.getMinutes();
+    }
+    else {
+      minute = newDate.getMinutes()
+    }
+  
+    // Create the date variable that is used in the start of the filename.
+    var dateOfReport = year + "-" + month + "-" + day;
+  
+    var filename = dateOfReport
+        + " " 
+        + classification 
+        + " MIBN LRRP " 
+        + from 
+        + " O-05 "
+        + dtgOfObservation + "Z";
+  
+    // Create a Blob object with the file content
+    var file = new Blob([fileContent], { type: "text/plain" });
+  
+    // Create a download link for the file, and set the filename to the filename variable
+    var downloadLink = document.createElement("a");
+    downloadLink.href = URL.createObjectURL(file);
+    downloadLink.download = filename;
+    downloadLink.style.display = "none";
+  
+    // Add the download link to the body and click it to start the download
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    
+    // Remove the link element from the body
+    document.body.removeChild(downloadLink);
+  }
